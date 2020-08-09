f6f4e688efca45618bd5320b87aeec49d79731bf
Yuriy Syrovetskiy <cblp@cblp.su>
2020-08-09 13:12:50 +0300

    Add parsing and printing

--- Equivalence.hs
+++ Equivalence.hs
@@ -1,4 +1,36 @@
 import           RIO
 
+import           Data.Bifunctor (Bifunctor, bimap)
+import           Data.Generics.Text (gshow)
+import           DynFlags (defaultDynFlags)
+import           Language.Haskell.GhclibParserEx.Config (fakeLlvmConfig,
+                                                         fakeSettings)
+import           Language.Haskell.GhclibParserEx.GHC.Parser (parseFile)
+import           Lexer (ParseResult (..), getErrorMessages, getMessages)
+import qualified RIO.Text as Text
+import           Text.Pretty.Simple (pPrintString)
+
 main :: IO ()
-main = pure ()
+main =
+  do
+    content <- readFileUtf8 file
+    pPrintString $ showParseResult $ parseFile file dynFlags $
+      Text.unpack content
+  where
+    file = "Equivalence.hs"
+
+    dynFlags = defaultDynFlags fakeSettings fakeLlvmConfig
+
+    showParseResult = \case
+      POk state result ->
+        "POk {state_messages = "
+        ++ show (bimapEach toList $ getMessages state dynFlags)
+        ++ ", result = " ++ gshow result ++ "}"
+      PFailed state ->
+        "PFailed{state_messages = "
+        ++ show (bimapEach toList $ getMessages state dynFlags)
+        ++ ", state_errorMessages = "
+        ++ show (toList $ getErrorMessages state dynFlags) ++ "}"
+
+bimapEach :: Bifunctor bi => (a -> b) -> bi a a -> bi b b
+bimapEach f = bimap f f
